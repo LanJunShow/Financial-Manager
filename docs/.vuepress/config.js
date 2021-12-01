8597d3bdb423289504270f8cc38ce6048abb9fb1
LanJunShow <38460616+LanJunShow@users.noreply.github.com>
2021-12-01 12:39:38 +0800

    修改文档首页说明

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,13 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
-  //解决github部署404问题
-  base:'/Financial-Manager/',
-  // theme: 'vdoing', // 使用npm包主题
-  theme: 'vdoing', // 使用docs主题
+  //解决github部署404问题 base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  // base:'/Financial-Manager/',
+  theme: 'vdoing', // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "Evan's blog",
-  description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
-  // base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  title: "理财管理",
+  description: '包含整个项目流程周期，涉及到需求、设计图、前端、后端、部署、测试、运维等知识点',
   markdown: {
     lineNumbers: true, // 代码行号
   },
